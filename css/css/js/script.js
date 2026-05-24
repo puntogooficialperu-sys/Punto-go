fecf7c3210e9b1643baa6f6f89fe76a91e822b8f
puntogooficialperu-sys <puntogooficial.peru@gmail.com>
2026-05-24 17:08:43 -0500

    Update script.js

--- css/css/js/script.js
+++ css/css/js/script.js
@@ -1,1 +1,37 @@
 
+let carrito = [];
+
+function agregar(nombre,precio){
+
+carrito.push({
+nombre:nombre,
+precio:precio
+});
+
+alert(nombre + " agregado");
+
+}
+
+function pedidoWhatsapp(){
+
+let mensaje="Hola Punto Go%0A";
+
+let total=0;
+
+carrito.forEach(item=>{
+
+mensaje += item.nombre+
+" S/"+item.precio+
+"%0A";
+
+total += item.precio;
+
+});
+
+mensaje += "%0ATotal: S/"+total;
+
+window.open(
+"https://wa.me/51999999999?text="+mensaje
+);
+
+}
